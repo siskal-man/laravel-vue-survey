0906500723765959b2ca172bcbef6c6c5e2ee4ae
siskal-man <block-code@mail.ru>
2022-04-09 15:39:31 +0300

    complete adding vuex and router

--- vue/src/main.js
+++ vue/src/main.js
@@ -1,8 +1,9 @@
 import { createApp } from 'vue';
 import store from './store';
+import router from './router';
 
 import './index.css';
 
 import App from './App.vue';
 
-createApp(App).use(store).mount('#app');
+createApp(App).use(router).use(store).mount('#app');
