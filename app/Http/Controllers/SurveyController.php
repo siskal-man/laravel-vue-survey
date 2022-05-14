b95755611e58643451bafebe4f5e9069eb5bf99c
siskal-man <block-code@mail.ru>
2022-05-14 17:02:15 +0300

    crud section complete

---
+++ app/Http/Controllers/SurveyController.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreSurveyRequest;
+use App\Http\Requests\UpdateSurveyRequest;
+use App\Http\Resources\SurveyResource;
+use App\Models\Survey;
+use Illuminate\Http\Request;
+
+class SurveyController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index(Request $request)
+    {
+        $user = $request->user();
+
+        return SurveyResource::collection(Survey::where("user_id", $user->id)->paginate());
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \App\Http\Requests\StoreSurveyRequest  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(StoreSurveyRequest $request)
+    {
+        $result = Survey::create($request->validate());
+
+        return new SurveyResource($request);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Survey  $survey
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Survey $survey, Request $request)
+    {
+        $user = $request->user();
+
+        if ($user->id !== $request->user_id) {
+            return abort(code: 403, message: "Unauthorized action.");
+        }
+
+        return new SurveyResource($survey);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \App\Http\Requests\UpdateSurveyRequest  $request
+     * @param  \App\Models\Survey  $survey
+     * @return \Illuminate\Http\Response
+     */
+    public function update(UpdateSurveyRequest $request, Survey $survey)
+    {
+        $survey->update($request->validate());
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Survey  $survey
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Survey $survey, Request $request)
+    {
+        $user = $request->user();
+        if ($user->id !== $survey->user_id) {
+            return abort(code: 403, message: "Unauthorized action.");
+        }
+        $survey->delete();
+        return response(content: "", status: 204);
+    }
+}
